7063f7c1e5edda5e36d17bfdbaafbf73c0ebae44
kevin <k.c.liu@accenture.com>
2017-12-21 19:30:56 +0800

    [sprint1] update pom

--- ai-voice/src/main/java/com/accenture/ai/AIVOICEApplication.java
+++ ai-voice/src/main/java/com/accenture/ai/AIVOICEApplication.java
@@ -1,7 +1,9 @@
 package com.accenture.ai;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class AIVOICEApplication {
